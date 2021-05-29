1acc27455e42d6423e03715a8eafae57f6a86dc5
Saurabh <saurabh2409899@gmail.com>
2021-05-29 15:23:28 +0530

    first commit

---
+++ notes/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+class Signup(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    contact=models.CharField(max_length=10)
+    branch = models.CharField(max_length=30)
+    role = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.user.username
+
+class Notes(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    uploadingdate=models.CharField(max_length=10)
+    branch = models.CharField(max_length=30)
+    subject = models.CharField(max_length=30)
+    notesfile = models.FileField(null=False)
+    filetype = models.CharField(max_length=30)
+    description = models.CharField(max_length=200, null=True)
+    status = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.Signup.user.username+" "+self.status
